bebad1df3a8fa2cd120ba8062448431a686d5ef6
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-02-20 15:59:10 +0200

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,14 @@ import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import About from './pages/About';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
