27d20e914e249fdb35c74104cdda22fb63f00fe4
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-02-22 17:33:45 +0200

    Add update user API route

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoutes from '../api/routes/user.route.js';
 import authRoutes from '../api/routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("MongoDb is Connected");
 }).catch((err) => {
