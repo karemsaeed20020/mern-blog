83c86c169159f492b50cca4f1cf245c4bfbddee7
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-02-24 19:33:45 +0200

    Add create a post API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoutes from '../api/routes/user.route.js';
 import authRoutes from '../api/routes/auth.route.js';
+import postRoutes from '../api/routes/post.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.listen('4000', () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
