4a0610647b46d8a06694bd74c417b227237a480a
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-03-14 19:51:12 +0200

    Complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,28 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react"
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import {AiOutlineSearch} from 'react-icons/ai';
 import {FaMoon, FaSun} from 'react-icons/fa';
 import {  useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../redux/theme/themeSlice";
 import { signoutSuccess } from "../redux/user/userSlice";
+import { useEffect, useState } from "react";
 
 const Header = () => {
     const path = useLocation().pathname;
+    const location = useLocation();
     const {currentUser} = useSelector(state => state.user);
     const {theme} = useSelector((state) => state.theme);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [searchTerm, setSearchTerm] = useState('');
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const searchTermFromUrl = urlParams.get('searchTerm');
+      if (searchTermFromUrl) {
+        setSearchTerm(searchTermFromUrl);
+      }
+    }, [location.search]);
+
     const handleSignOut = async () => {
       try {
         const res = await fetch('/api/user/signout', {
@@ -27,28 +39,37 @@ const Header = () => {
         console.log(error.message);
       }
     }
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const urlParams = new URLSearchParams(location.search);
+      urlParams.set('searchTerm', searchTerm);
+      const searchQuery = urlParams.toString();
+      navigate(`/search?${searchQuery}`);
+    };
+  
   return (
     <Navbar className="border-b-2">
       <Link to={'/'} className="self-center dark:text-white whitespace-nowrap text-sm sm:text-xl font-semibold">
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg">Kareem</span>
         Blog
       </Link>
-      <form action="">
-        <TextInput type="text" placeholder="Search..." rightIcon={AiOutlineSearch} className="hidden lg:inline" />
+      <form onSubmit={handleSubmit}>
+        <TextInput type="text" placeholder="Search..." rightIcon={AiOutlineSearch} className="hidden lg:inline" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
       <Button
-    className="w-12 h-10 hidden sm:inline"
-    pill
-    color="gray"
-    onClick={() => {
-        console.log("Button clicked");
-        dispatch(toggleTheme());
-    }}
->            {theme === 'light' ? <FaSun /> : <FaMoon />}
+        className="w-12 h-10 hidden sm:inline"
+        pill
+        color="gray"
+        onClick={() => {
+            console.log("Button clicked");
+            dispatch(toggleTheme());
+        }}
+        >   
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
         {currentUser ? (
           <Dropdown arrowIcon={false} inline label={<Avatar alt="user" img={currentUser.profilePicture} rounded />}>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import './index.css';
 import { store, persistor  } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
-    <Provider store={store}>
+  <Provider store={store}>
+    <ThemeProvider>
       <App />
-    </Provider>
-  </PersistGate>
+    </ThemeProvider>
+  </Provider>
+</PersistGate>
 )
