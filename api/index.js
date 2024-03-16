9d74a7886b6c10a46852907dd5496d4e7a53a336
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-03-16 18:49:12 +0200

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from '../api/routes/auth.route.js';
 import postRoutes from '../api/routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 const app = express();
@@ -17,6 +18,8 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 })
 
+const __dirname = path.resolve();
+
 
 app.listen('4000', () => {
     console.log('Server is running on port 4000!');
@@ -25,7 +28,12 @@ app.listen('4000', () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
-app.use('/api/comment', commentRoutes)
+app.use('/api/comment', commentRoutes);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
